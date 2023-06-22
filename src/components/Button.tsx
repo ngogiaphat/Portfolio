ff785d7db6a872ba32fe3bd187713128935983d8
Samuel Seo <ngogiaphat0802@gmail.com>
2023-06-22 12:27:21 +0700

    started working on Navbar

---
+++ src/components/Button.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Link from 'next/link';
+function Button({text, link} : {text: string; link: string}){
+	return(
+		<Link className="btn" href={link}>
+			{text}
+		</Link>
+	);
+};
+export default Button;
