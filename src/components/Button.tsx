bab368a9868eb7e5a496841ee975c6cb5b04b075
Samuel Seo <ngogiaphat0802@gmail.com>
2023-06-30 16:19:20 +0700

    line space

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Link from 'next/link';
-function Button({text, link} : {text: string; link: string}){
+function Button({text, link} : {
+	text: string; 
+	link: string
+}){
 	return(
 		<Link className="btn" href={link}>
 			{text}
